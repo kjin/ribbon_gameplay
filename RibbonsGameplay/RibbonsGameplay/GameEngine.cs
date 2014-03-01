28af0eb60b6627601fd575b2b2a9b0255bd33a87
raginkjin <kkj9@cornell.edu>
2014-03-01 17:48:12 -0500

    cleared up reference issues

--- RibbonsGameplay/RibbonsGameplay/GameEngine.cs
+++ RibbonsGameplay/RibbonsGameplay/GameEngine.cs
@@ -16,116 +16,112 @@ using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
 //using Microsoft.Xna.Framework.Net;
 //using Microsoft.Xna.Framework.Storage;
-using RibbonsGameplay.GameCanvas;
 #endregion
 
 namespace RibbonsGameplay
 {
-    class GameEngine
-    {
-        /// <summary>
+    /// <summary>
     /// The primary controller class for an XNA game.
     /// </summary>
-        public class GameEngine : Microsoft.Xna.Framework.Game
+    public class GameEngine : Microsoft.Xna.Framework.Game
+    {
+
+        #region Constants
+        // How many frames after winning/losing do we continue?
+        int COUNTDOWN = 60;
+        #endregion
+
+        #region Fields
+        // Win/Lose countdown (SIMPLE FIELD)
+        int countdown;
+
+        // Used to draw the game onto the screen (VIEW CLASS)
+        protected GameCanvas canvas;
+
+        // Victory and Failure Textures (part of VIEW)
+        private Texture2D victory;
+        private Texture2D failure;
+
+        // Used to load the sounds and graphics (CONTROLLER CLASS)
+        protected ContentManager content;
+
+        // Read input from keyboard or game pad (CONTROLLER CLASSES)
+        protected MainInputController inputController;
+
+        #endregion
+
+        #region Initialization
+        /// <summary>
+        ///  Constructor to create a new instance of our game.
+        /// </summary>
+        public GameEngine()
         {
+            // Tell the program to load all files relative to the "Content" directory.
+            content = new ContentManager(Services);
+            content.RootDirectory = "Content";
+            canvas = new GameCanvas(this);
+        }
 
-            #region Constants
-            // How many frames after winning/losing do we continue?
-            int COUNTDOWN = 60;
-            #endregion
+        /// <summary>
+        /// Preform any initialization necessary before running the game.  This
+        /// includes both (preliminary) model and view creation.
+        /// </summary>
+        protected override void Initialize()
+        {
+            canvas.Initialize(this);
+            //inputController = new MainInputController();
+            base.Initialize();
+        }
 
-            #region Fields
-            // Win/Lose countdown (SIMPLE FIELD)
-            int countdown;
-
-            // Used to draw the game onto the screen (VIEW CLASS)
-            protected GameCanvas canvas;
-
-            // Victory and Failure Textures (part of VIEW)
-            private Texture2D victory;
-            private Texture2D failure;
-
-            // Used to load the sounds and graphics (CONTROLLER CLASS)
-            protected ContentManager content;
-
-            // Read input from keyboard or game pad (CONTROLLER CLASSES)
-            protected MainInputController inputController;
-
-            #endregion
-
-            #region Initialization
-            /// <summary>
-            ///  Constructor to create a new instance of our game.
-            /// </summary>
-            public GameEngine()
-            {
-                // Tell the program to load all files relative to the "Content" directory.
-                content = new ContentManager(Services);
-                content.RootDirectory = "Content";
-                canvas = new GameCanvas(this);
-            }
-
-            /// <summary>
-            /// Preform any initialization necessary before running the game.  This
-            /// includes both (preliminary) model and view creation.
-            /// </summary>
-            protected override void Initialize()
-            {
-                canvas.Initialize(this);
-                //inputController = new MainInputController();
-                base.Initialize();
-            }
-
-            /// <summary>
-            /// Load all graphic and audio content (this game has no audio).
-            /// </summary>
-            protected override void LoadContent()
-            {
-                // General view content
-                canvas.LoadContent(content);
-                victory = content.Load<Texture2D>("Shared\\victory");
-                failure = content.Load<Texture2D>("Shared\\failure");
-
-            }
-
-            /// <summary>
-            /// Unload all graphic and audio content.
-            /// </summary>
-            protected override void UnloadContent()
-            {
-                // Content managers make this step easy.
-                content.Unload();
-            }
-            #endregion
-
-            #region Game Loop
-            /// <summary>
-            /// Read user input, calculate physics, and update the models.
-            /// </summary>
-            /// <param name="gameTime">Provides a snapshot of timing values.</param>
-            protected override void Update(GameTime gameTime)
-            {
-                inputController.ReadInput();
-
-                // Adjust world position if necessary.
-                bool reset = inputController.Reset;
-                bool next = inputController.Next;
-
-                base.Update(gameTime);
-            }
-
-            /// <summary>
-            /// This is called when the game should draw itself.
-            /// </summary>
-            /// <param name="gameTime">Provides a snapshot of timing values.</param>
-            protected override void Draw(GameTime gameTime)
-            {
-                canvas.Reset();
-
-                base.Draw(gameTime);
-            }
-            #endregion
+        /// <summary>
+        /// Load all graphic and audio content (this game has no audio).
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // General view content
+            canvas.LoadContent(content);
+            victory = content.Load<Texture2D>("Shared\\victory");
+            failure = content.Load<Texture2D>("Shared\\failure");
 
         }
+
+        /// <summary>
+        /// Unload all graphic and audio content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // Content managers make this step easy.
+            content.Unload();
+        }
+        #endregion
+
+        #region Game Loop
+        /// <summary>
+        /// Read user input, calculate physics, and update the models.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            inputController.ReadInput();
+
+            // Adjust world position if necessary.
+            bool reset = inputController.Reset;
+            bool next = inputController.Next;
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            canvas.Reset();
+
+            base.Draw(gameTime);
+        }
+        #endregion
+
     }
 }
--- RibbonsGameplay/RibbonsGameplay/MainInputController.cs
+++ RibbonsGameplay/RibbonsGameplay/MainInputController.cs
@@ -22,7 +22,6 @@
 using System;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
-using Microsoft.Xna.Framework.Net;
 #endregion
 
 /// <summary>
