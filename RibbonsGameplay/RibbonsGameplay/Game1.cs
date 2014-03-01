685829464da04d3738cc5228091d0a1bca50c01a
raginkjin <kkj9@cornell.edu>
2014-02-28 23:48:58 -0500

    Commiting a lot of things???

--- RibbonsGameplay/RibbonsGameplay/Game1.cs
+++ RibbonsGameplay/RibbonsGameplay/Game1.cs
@@ -81,7 +81,7 @@ namespace RibbonsGameplay
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.Black);
+            GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
 
